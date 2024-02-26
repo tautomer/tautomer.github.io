0243550954e1e75249e464ba11233be3408b6b64
Xinyang Li <soybeanyoung@gmail.com>
2024-02-26 02:47:01 -0700

    Multiple updates
    1. A new bash script for organizing photos
    2. Update astro photos
    3. Add animal photos
    4. Add a back button to the navigation bar
    5. Fix several typos

---
+++ images/photography/organize_photos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=${PWD##*/}
+yaml=$dir.yml
+echo -e "picture_path: $dir\npicutures:" > $yaml
+
+cd originals
+for i in *.jpg
+do
+    basename=${i%.*}
+    echo "- filename: $basename" >> ../$yaml
+    sizes=$(identify -ping -format '%wx%h' $i)
+    # copy original to ../original-wxh.jpg
+    cp $i ../$basename-$sizes.jpg  
+    echo "  original: $basename-$sizes.jpg" >> ../$yaml
+    # resize to half
+    convert $i -resize 50% tmp.jpg
+    sizes=$(identify -ping -format '%wx%h' tmp.jpg)
+    mv tmp.jpg ../$basename-$sizes.jpg  
+    echo -e "  sizes:\n  - $basename-$sizes.jpg" >> ../$yaml
+    # create a thumbnail
+    convert $i -resize 1000x1000 tmp.jpg
+    mv tmp.jpg ../$basename-thumbnail.jpg  
+    echo "  thumbnail: $basename-thumbnail.jpg" >> ../$yaml
+done
---
+++ photography/animals.md
@@ -0,0 +1,13 @@
+---
+layout: gallery
+title: Animals
+support: [jquery, gallery]
+---
+
+Pictures of both wild and domestic animals I have encountered during daily life,
+traveling, or hiking.
+
+Warning: there is a photo of a rattlesnake. Maybe it is already to late when you
+see this warning...
+
+{% include gallery-layout.html gallery=site.data.galleries.animals %}
